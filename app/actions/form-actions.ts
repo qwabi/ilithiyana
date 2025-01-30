57ee959de6e1c56c3c33d170598c06291a762427
Ayabonga Qwabi <ayabongaqwabi@gmail.com>
2025-01-30 19:15:54 +0200

    save date as iso

--- app/actions/form-actions.ts
+++ app/actions/form-actions.ts
@@ -3,9 +3,13 @@
 import { put } from '@vercel/blob';
 import { revalidatePath } from 'next/cache';
 import { putSubmission } from '@/lib/blob-storage';
+import { date } from 'zod';
 
 export async function submitFormData(type: string, formData: any) {
-  const jsonData = JSON.stringify(formData);
+  const jsonData = JSON.stringify({
+    ...formData,
+    date: new Date().toISOString(),
+  });
 
   try {
     const result = await putSubmission(type, jsonData);
